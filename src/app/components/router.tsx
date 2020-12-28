43c92c2a21a55939f3f7bf9a383fbd8e1f043e2c
Nikolaev Petr <PANikolaev@sberbank.ru>
2020-12-28 09:16:00 +0300

    Create simple scene with sphere

--- src/app/components/router.tsx
+++ src/app/components/router.tsx
@@ -13,7 +13,6 @@ import BlogImitation from './algoritms/blog-imitation';
 import { HardUILogicApp } from './hard-logic-ui-components/app';
 import thunk from 'redux-thunk';
 import { rootReducer } from './hard-logic-ui-components/custom-redux/reducers/rootReducer';
-import { GraphicContainer } from './universe/graphic-contaner';
 
 // const store = createStore(rootReducer);
 
@@ -60,7 +59,6 @@ export class RouterComponent extends React.Component<{}, State> {
 				<Provider store={store}>
 					<Route path={'/apps/hard-ui-logic'} render={() => <HardUILogicApp title={'Super component'} />} />
 				</Provider>
-				<Route path='/universe' component={GraphicContainer} />
 			</Router>
 		);
 	}
--- src/app/components/universe/index.ts
+++ src/app/components/universe/index.ts
@@ -1,5 +1,51 @@
-import planet from '@models/planet.png';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-console.log('Hello the future universe system!');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
 
-console.log(planet);
+new OrbitControls(camera, renderer.domElement);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
+
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const texture = new THREE.TextureLoader().load( 'models/sun.png' );
+const material = new THREE.MeshBasicMaterial( { map: texture, wireframe: false } );
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+const geometry2 = new THREE.SphereGeometry(2, 32, 32);
+const material2 = new THREE.MeshBasicMaterial( { color: 0x53a14b, wireframe: true } );
+const sphere2 = new THREE.Mesh(geometry2, material2);
+scene.add(sphere2);
+
+camera.position.z = 5;
+
+// game logic
+const update = () => {
+}
+
+// draw scene
+const render = () => {
+  renderer.render(scene, camera);
+}
+
+// run game loop (update, render, repeat)
+const gameLoop = () => {
+  requestAnimationFrame(gameLoop);
+
+  update();
+  render();
+}
+
+gameLoop();
