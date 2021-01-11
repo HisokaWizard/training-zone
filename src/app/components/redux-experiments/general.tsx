2678795f209cdcd4e12e2abb01144fdaa951e1e2
Nikolaev Petr <PANikolaev@sberbank.ru>
2021-01-11 11:06:24 +0300

    Start redux experiments for redux saga and the other moments.

---
+++ src/app/components/redux-experiments/general.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const GeneralRedux = () => {
+  return (
+    <div>
+      General redux!!!
+    </div>
+  )
+}
--- src/app/components/router.tsx
+++ src/app/components/router.tsx
@@ -3,23 +3,15 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { StartPage } from './startPage';
 import { BinarySearch } from './algoritms/binary-search';
 import { AllSort } from './algoritms/sort';
+import { GeneralRedux } from './redux-experiments/general';
 
-export class RouterComponent extends React.Component<{}, {}> {
-
-	constructor(props: {}) {
-		super(props);
-		this.state = {
-		};
-	}
-
-	render() {
-		const {} = this.state;
-		return (
-			<Router>
-				<Route exact path={'/'} component={StartPage} />
-				<Route path={'/algorithms/sort'} component={AllSort} />
-				<Route path={'/algorithms/binary-search'} component={BinarySearch} />
-			</Router>
-		);
-	}
+export const RouterComponent = () => {
+	return (
+		<Router>
+			<Route exact path={'/'} component={StartPage} />
+			<Route path={'/algorithms/sort'} component={AllSort} />
+			<Route path={'/algorithms/binary-search'} component={BinarySearch} />
+			<Route path={'/redux-experiments'} component={GeneralRedux} />
+		</Router>
+	);
 }
